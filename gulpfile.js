44776a0e5b2e7eceaf16cdbfed3ed1b705249200
aynap <aynap.learning@gmail.com>
2016-04-09 20:34:37 +0700

    Add gulp and browsersyn

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-var gulp = require('gulp');
+// var gulp        = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var sass = require('gulp-ruby-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// gulp.task('default', function() {
+//     gulp.watch(['*.html'], browserSync.reload);
+//     gulp.watch(['css/**/*.css'], browserSync.reload);
+// });
+
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+// });
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Save a reference to the `reload` method
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
 });
